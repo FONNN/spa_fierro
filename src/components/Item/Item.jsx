0d7a7a63cb900854dcacad47865168ec2be480ff
Alonso <robertofierrourrutia@gmail.com>
2022-08-24 14:25:45 -0400

    integracion bootstrap grid para item e itemList

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,22 +6,57 @@ import { Link } from 'react-router-dom'
 const Item = memo(
   ({ products }) => {
     return (
-      <div className='col-md-3 m-3'>
-        <Card style={{ maxWidth: '20rem' }}>
-          <Card.Img className='w-50' variant="top" src={products.image} />
-          <Card.Body>
-            <Card.Title>
-              {`${products.name} - ${products.category}`}
-            </Card.Title>
-            <Card.Text>
-              {`Precio: ${products.price}`}
-            </Card.Text>
+
+      <div className="col">
+        <div className="card h-100">
+          <img src={products.image} className="card-img-top img-fluid" alt="imagen de tarjeta" />
+          <div className="card-body">
+            <h5 className="card-title">{products.name}</h5>
+            <p className="card-text">{products.description}</p>
             <Link to={`/detail/${products.id}`}>
-              <Button variant="info">Ver detalle</Button>
+              <button className="btn btn-outline-dark">ver detalles</button>
             </Link>
-          </Card.Body>
-        </Card>
+          </div>
+        </div>
       </div>
+
+      // <div className='col-md-3 m-3'>
+      //   <Card style={{ maxWidth: '20rem' }}>
+      //     <Card.Img className='w-50' variant="top" src={products.image} />
+      //     <Card.Body>
+      //       <Card.Title>
+      //         {`${products.name} - ${products.category}`}
+      //       </Card.Title>
+      //       <Card.Text>
+      //         {`Precio: ${products.price}`}
+      //       </Card.Text>
+      //       <Link to={`/detail/${products.id}`}>
+      //         <Button variant="outline-dark">Ver detalle</Button>
+      //       </Link>
+      //     </Card.Body>
+      //   </Card>
+      // </div>
+
+      // <div className="card h-100 shadow-sm">
+      //   <img
+      //     src={products.image}
+      //     className="card-img-top" alt={products.name}
+      //   />
+      //   <div className="card-body">
+      //     <div className="clearfix mb-3">
+      //       <span className="float-start badge rounded-pill bg-info">{products.name}</span>
+      //       <span className="float-end price-hp">{products.price}</span>
+      //     </div>
+      //     <h5 className="card-title">
+      //       {products.description}
+      //     </h5>
+      //     <div className="text-center my-4">
+      //       <Link to={`/detail/${products.id}`}>
+      //         <button type="button" className="btn btn-secondary">Ver detalle</button>
+      //       </Link>
+      //     </div>
+      //   </div>
+      // </div>
 
       // <div className="card" style={{width: '400px'}}>
       //   <img className="card-img-top" src={products.image} alt="Card image" style={{width: '100%'}} />
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react'
 import Item from '../Item/Item';
 
-import CardGroup from 'react-bootstrap/CardGroup';
+import { Container, CardGroup, Col, Row} from 'react-bootstrap';
 
 // import { CardGroup } from 'react-bootstrap';
 
@@ -9,25 +9,26 @@ const ItemList = memo(
     ({ products }) => {
         console.log(products)
         return (
-            <>
-                <CardGroup>
-                    {
-                        products?.map(product => <Item key={product.id} products={product} />)
-                    }
-                </CardGroup>
-            </>
+            // <Container>
+            //     <CardGroup>
+            //         {
+            //             products?.map(product => <Item key={product.id} products={product} />)
+            //         }
+            //     </CardGroup>
+            // </Container>
+    <div className="container">
+
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+            
+                {
+                    products?.map(product => <Item key={product.id} products={product} />)
+                }
+            
+        </div>
+
+    </div>
         )
     }
 )
-// <div className="container text-center">
-
-// <div className="row row-cols-1 row-cols-md-2 g-4">
-//     <div className="col">
-//         {
-//             productos?.map(products => <Item key={products.id} products={products} />)
-//         }
-//     </div>
-// </div>
-// </div>
 
 export default ItemList
